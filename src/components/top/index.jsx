3243f451aad39751914704d8329c19821cbef15d
kevinGwon <developerGwon@gmail.com>
2020-01-29 02:31:05 +0900

    Update top - navigator

--- src/components/top/index.jsx
+++ src/components/top/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { GitHubIcon } from '../social-share/github-icon'
+import { Layout } from '../../layout'
 
 import './index.scss'
 
@@ -8,11 +9,21 @@ export const Top = ({ title, location, rootPath }) => {
   const isRoot = location.pathname === rootPath
   return (
     <div className="top">
-      {!isRoot && (
-        <Link to={`/`} className="link">
-          {title}
-        </Link>
-      )}
+      <nav className="top-nav">
+        <h1 className="a11y">KevinGwon Navigator</h1>
+        <ul>
+          <li>
+            <Link to={`/`} className="link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to={`/blog`} className="link">
+              Blog
+            </Link>
+          </li>
+        </ul>
+      </nav>
       <GitHubIcon />
     </div>
   )
