14a520aa9e4b2550163dace2ea3f4d42682b15dc
kevinGwon <developerGwon@gmail.com>
2020-01-29 02:28:45 +0900

    Fix footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -3,7 +3,5 @@ import React from 'react'
 import './index.scss'
 
 export const Footer = () => (
-  <footer className="footer">
-    ©kevinGwon
-  </footer>
+  <footer className="footer">© 2020, kevinGwon, Made with Gatsuby</footer>
 )
