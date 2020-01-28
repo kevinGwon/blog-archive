3b2befe3880b62482a0569ffea6b3a11fc9ac467
kevinGwon <developerGwon@gmail.com>
2020-01-29 02:30:11 +0900

    Fix bio - 링크 해제

--- src/components/bio/index.jsx
+++ src/components/bio/index.jsx
@@ -9,7 +9,6 @@ export const Bio = () => (
     query={bioQuery}
     render={data => {
       const { author, social, introduction } = data.site.siteMetadata
-
       return (
         <div className="bio">
           <div className="author">
@@ -24,9 +23,9 @@ export const Bio = () => (
               />
               <div className="author-name">
                 <span className="author-name-prefix">Written by</span>
-                <Link to={'/about'} className="author-name-content">
+                <span className="author-name-content">
                   <span>@{author}</span>
-                </Link>
+                </span>
                 <div className="author-introduction">{introduction}</div>
                 <p className="author-socials">
                   {social.github && (
